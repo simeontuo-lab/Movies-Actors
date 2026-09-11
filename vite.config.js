764357a452e4a07d58beacca7c9df2fab63eeeba
simeontuo-lab <simeon.tuo@gmail.com>
2026-09-10 21:52:42 -0700

    5

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-
-  base: "./Movies-Actors/",
-  
+  base: "/Movies-Actors/",
   plugins: [react()],
 });
-
